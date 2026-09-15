92883940beaa2c31ae0e5d629cf195d65f9e2dc1
Brendon Nhachi <brendon9624@gmail.com>
2026-09-15 07:57:25 +0000

    added structs to store pin number

--- projects/01-blink/components/led/led.c
+++ projects/01-blink/components/led/led.c
@@ -1,31 +1,27 @@
 #include "led.h"
-#include "esp_log.h"
-#include "driver/gpio.h"
 
 static const char *TAG = "LED_ON_OFF";
 
-esp_err_t led_init(int GPIO_PIN_NUMBER)
+esp_err_t led_init(led_config_t *led_config)
 {
     gpio_config_t io_config = { 
-    .pin_bit_mask =  (1ULL << GPIO_PIN_NUMBER),
+    .pin_bit_mask =  (1ULL << led_config->pin),
     .mode = GPIO_MODE_OUTPUT,
     .pull_up_en = GPIO_PULLUP_DISABLE,
     .pull_down_en = GPIO_PULLDOWN_DISABLE,
     .intr_type = GPIO_INTR_DISABLE,
     };
     return gpio_config(&io_config);
-    
-    
 }
 
-void led_on(int GPIO_PIN_NUMBER)
+void led_on(led_config_t *led_config)
 {
-    gpio_set_level(GPIO_PIN_NUMBER,IO_ON);
+    gpio_set_level(led_config->pin,IO_ON);
     ESP_LOGI(TAG, "LED IS ON");
 }
 
-void led_off(int GPIO_PIN_NUMBER)
+void led_off(led_config_t *led_config)
 {
-    gpio_set_level(GPIO_PIN_NUMBER,IO_OFF);
+    gpio_set_level(led_config->pin,IO_OFF);
     ESP_LOGI(TAG, "LED IS OFF");
 }
--- projects/01-blink/main/01-blink.c
+++ projects/01-blink/main/01-blink.c
@@ -2,16 +2,18 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-#define IO_PIN GPIO_NUM_2
+
+
 void app_main(void)
 {
     TickType_t delay_ticks = pdMS_TO_TICKS(1000);
-    if (led_init(IO_PIN) == ESP_OK)
+    led_config_t led = {.pin = GPIO_NUM_2};
+    if (led_init(&led) == ESP_OK)
     {
         while (true){
-            led_on(IO_PIN);
+            led_on(&led);
             vTaskDelay(delay_ticks);
-            led_off(IO_PIN);
+            led_off(&led);
             vTaskDelay(delay_ticks);
         }
     }
